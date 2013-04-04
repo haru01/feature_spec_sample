2b9cdd032989be1b5a6c36747f17a370583f9289
eiji.ienaga <eiji.ienaga@gmail.com>
2013-04-04 16:34:03 +0900

    init

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+FeatureSpecSample::Application.config.session_store :cookie_store, key: '_feature_spec_sample_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# FeatureSpecSample::Application.config.session_store :active_record_store
