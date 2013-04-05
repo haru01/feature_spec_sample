3a8297275cf0dbb5fd92d657df4c22440521117c
eiji.ienaga <eiji.ienaga@gmail.com>
2013-04-05 16:14:40 +0900

    modify scenario name

--- spec/features/google_spec.rb
+++ spec/features/google_spec.rb
@@ -5,12 +5,14 @@ feature 'Google search' do
     Capybara.app_host = 'http://www.google.co.jp'
   end
 
-  scenario 'search' do
+  scenario 'search and visit detail' do
     visit '/'
+
     fill_in 'gbqfq', with: 'bdd wikipedia'
     click_button 'gbqfb'
 
     click_link 'Behavior-driven development - Wikipedia, the free encyclopedia'
+
     page.should have_content 'A ubiquitous language is a (semi-)formal language that is shared by all members of a software development team — both software developers and non-technical personnel.'
   end
 end
